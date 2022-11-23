649fa5685fb4f7fb5f43bcd65528be9807c813e4
revike <revike@ya.ru>
2022-11-23 10:35:54 +0300

    robots.txt

--- tex_up/main_app/urls.py
+++ tex_up/main_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from main_app import views as main_app
 
 app_name = 'main_app'
 
 urlpatterns = [
     path('', main_app.IndexView.as_view(), name='index'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt",
+                                            content_type="text/plain")),
 ]
